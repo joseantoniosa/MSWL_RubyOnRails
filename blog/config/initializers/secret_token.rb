440bdc4bedd853b22809ff9792aeee70ca2761dc
Jose Antonio S.A <jose.antonio.sa@gmail.com>
2012-10-03 00:56:58 +0200

    First commit. 1.3.3. Create the index action and view

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ec18660e59d9626556cd7b53f99e0aa3826ad823b15b6dafbfd0d56e9084d11f79efdeec72ded5f64d62c8c154c5e3d9fe4343ff2ec7740cbf3eb166a0326b81'
