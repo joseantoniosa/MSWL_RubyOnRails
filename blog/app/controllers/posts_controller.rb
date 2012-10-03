2ee2ba6ce295c69d204b7ad13e31d17f84fd8fef
Jose Antonio S.A <jose.antonio.sa@gmail.com>
2012-10-03 23:05:52 +0200

    1.3.8. Edit a post: the edit and update actions

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -11,11 +11,25 @@ class PostsController < ApplicationController
     end
     def create
         @post = Post.new(params[:post])
-        
+
         if @post.save
             redirect_to @post, :notice => 'Post created'
         else
             render 'new'
         end
     end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update_attributes(params[:post])
+            redirect_to @post, :notice => 'Post updated'
+        else
+            render 'edit'
+        end
+    end
+
 end  
